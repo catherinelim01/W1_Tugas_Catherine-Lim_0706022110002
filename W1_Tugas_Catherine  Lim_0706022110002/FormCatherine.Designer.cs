588bf821f03ec28bcc02b1af27e06a8382019b36
catherinelim01 <catherinelim01@student.ciputra.ac.id>
2022-02-27 22:15:15 +0700

    Add project files.

---
+++ W1_Tugas_Catherine  Lim_0706022110002/FormCatherine.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace W1_Tugas_Catherine__Lim_0706022110002
+{
+    partial class FormCatherine
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelCatherine = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LabelCatherine
+            // 
+            this.LabelCatherine.AutoSize = true;
+            this.LabelCatherine.Location = new System.Drawing.Point(119, 45);
+            this.LabelCatherine.Name = "LabelCatherine";
+            this.LabelCatherine.Size = new System.Drawing.Size(217, 25);
+            this.LabelCatherine.TabIndex = 0;
+            this.LabelCatherine.Text = "HI! My Name is Catherine.";
+            // 
+            // FormCatherine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 112);
+            this.Controls.Add(this.LabelCatherine);
+            this.Name = "FormCatherine";
+            this.Text = "Catherine Lim_0706022110002";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelCatherine;
+    }
+}
+
---
+++ W1_Tugas_Catherine  Lim_0706022110002/FormCatherine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W1_Tugas_Catherine__Lim_0706022110002
+{
+    public partial class FormCatherine : Form
+    {
+        public FormCatherine()
+        {
+            InitializeComponent();
+        }
+    }
+}
